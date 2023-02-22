1f6ca3f6e6d0ec807bce194a6cb2e6d5e719cae2
j1sk1ss <fotikolay222@gmail.com>
2023-02-22 20:15:04 +0500

    deleting Non-Npc loaner method added

--- src/main/java/economy/pcconomy/backend/bank/npc/Loaner.java
+++ src/main/java/economy/pcconomy/backend/bank/npc/Loaner.java
@@ -1,11 +1,25 @@
 package economy.pcconomy.backend.bank.npc;
 
+import com.palmergames.bukkit.towny.TownyAPI;
+import economy.pcconomy.PcConomy;
+import economy.pcconomy.backend.cash.scripts.CashWorker;
+import economy.pcconomy.backend.scripts.ItemWorker;
+import economy.pcconomy.backend.trade.npc.Trader;
 import economy.pcconomy.frontend.ui.Window;
 
+import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.event.NPCLeftClickEvent;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
 import net.citizensnpcs.api.trait.Trait;
 import net.citizensnpcs.api.trait.TraitName;
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerChatEvent;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.UUID;
 
 @TraitName("Loaner")
 public class Loaner extends Trait {
@@ -20,4 +34,36 @@ public class Loaner extends Trait {
 
         Window.OpenLoanWindow(player, false);
     }
+
+    @EventHandler
+    public void onInteraction(NPCLeftClickEvent event) {
+        if (!event.getNPC().equals(this.getNPC())) return;
+        var player = event.getClicker();
+        var playerUUID = player.getUniqueId();
+        var homeTown = TownyAPI.getInstance().getTownName(player.getLocation());
+
+        if (TownyAPI.getInstance().getTown(homeTown).getMayor().getUUID().equals(playerUUID)) {
+            player.sendMessage("Удалить кредитора? (y/n)");
+            chat.put(playerUUID, event.getNPC().getId());
+        }
+    }
+
+    private final Dictionary<UUID, Integer> chat = new Hashtable<>();
+
+    @EventHandler
+    public void Chatting(PlayerChatEvent event) {
+        var player = event.getPlayer();
+        var playerMessage = event.getMessage();
+        event.setCancelled(true);
+
+        if (chat.get(player.getUniqueId()) != null) {
+            var loaner = CitizensAPI.getNPCRegistry().getById(chat.get(player.getUniqueId()));
+            if (StringUtils.containsAny(playerMessage, "ynYN")) {
+                if (playerMessage.toLowerCase().contains("y")) {
+                    loaner.destroy();
+                }
+            }
+        }
+    }
+
 }
