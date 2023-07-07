51d41fb31489a914d2c12cf7ae024640ad990c15
j1sk1ss <fotikolay222@gmail.com>
2023-07-07 22:43:08 +0500

    bank menu changed

--- src/main/java/economy/pcconomy/frontend/ui/windows/bank/BankerListener.java
+++ src/main/java/economy/pcconomy/frontend/ui/windows/bank/BankerListener.java
@@ -1,7 +1,6 @@
 package economy.pcconomy.frontend.ui.windows.bank;
 
 import economy.pcconomy.PcConomy;
-import economy.pcconomy.backend.cash.CashManager;
 import economy.pcconomy.backend.scripts.items.ItemManager;
 import economy.pcconomy.frontend.ui.windows.Window;
 
@@ -19,24 +18,12 @@ public class BankerListener implements Listener {
 
         if (Window.isThisWindow(event, player, "Банк")) {
             var option = Objects.requireNonNull(event.getCurrentItem());
-            switch (event.getView().getTitle()) {
-                case "Банк" -> {
-                    switch (BankerWindow.Panel.click(event.getSlot()).getName()) {
-                        case "Внести деньги" -> player.openInventory(BankerWindow.putWindow(player));
-                        case "Вывести деньги" -> player.openInventory(BankerWindow.withdrawWindow(player));
-                    }
-                }
-                case "Банк-Снятие" -> {
-                    var amount = Double.parseDouble(ItemManager.getName(option).
-                            replace(CashManager.currencySigh, ""));
-                    PcConomy.GlobalBank.giveCashToPlayer(amount, player);
-                }
-                case "Банк-Внесение" -> {
-                    var amount = Double.parseDouble(ItemManager.getName(option).
-                            replace(CashManager.currencySigh, ""));
-                    PcConomy.GlobalBank.takeCashFromPlayer(Math.abs(amount), player);
-                }
-            }
+            var amount = ItemManager.getPriceFromLore(option, 0);
+
+            if (amount > 0)
+                PcConomy.GlobalBank.giveCashToPlayer(amount, player);
+            else
+                PcConomy.GlobalBank.takeCashFromPlayer(Math.abs(amount), player);
 
             event.setCancelled(true);
         }
--- src/main/java/economy/pcconomy/frontend/ui/windows/bank/BankerWindow.java
+++ src/main/java/economy/pcconomy/frontend/ui/windows/bank/BankerWindow.java
@@ -5,7 +5,6 @@ import economy.pcconomy.backend.cash.CashManager;
 import economy.pcconomy.backend.cash.ChangeManager;
 import economy.pcconomy.backend.scripts.BalanceManager;
 import economy.pcconomy.backend.scripts.items.Item;
-import economy.pcconomy.backend.scripts.items.ItemManager;
 import economy.pcconomy.frontend.ui.objects.Panel;
 import economy.pcconomy.frontend.ui.objects.interactive.Button;
 import economy.pcconomy.frontend.ui.windows.IWindow;
@@ -21,54 +20,47 @@ import org.bukkit.inventory.ItemStack;
 import java.util.Arrays;
 
 public class BankerWindow implements IWindow {
-    public static final economy.pcconomy.frontend.ui.objects.Panel Panel = new Panel(Arrays.asList(
-            new Button(Arrays.asList(
-                    0, 1, 2, 3, 9, 10, 11, 12, 18, 19, 20, 21
-            ), "Внести деньги", ""),
-            new Button(Arrays.asList(
-                    5, 6, 7, 8, 14, 15, 16, 17, 23, 24, 25, 26
-            ), "Вывести деньги", "")
-    ));
-
-    public Inventory generateWindow(Player player) {
-        return Panel.placeComponents(Bukkit.createInventory(player, 27, Component.text("Банк")));
-    }
-
-    public static Inventory withdrawWindow(Player player) {
-        var window = Bukkit.createInventory(player, 9, Component.text("Банк-Снятие"));
+    public Inventory generateWindow(Player player) { // 9 -> 26
+        var window = Bukkit.createInventory(player, 54, Component.text("Банк"));
 
         var enableBalance   = PcConomy.GlobalBank.getUsefulAmountOfBudget();
         var playerBalance   = new BalanceManager().getBalance(player);
+        var cashInInventory = new CashManager().amountOfCashInInventory(player);
 
-        for (var i = 0; i < 9; i++) {
+        var textBalance = playerBalance + "";
+        for (var i = 9; i < Math.min(textBalance.toCharArray().length + 9, 27); i++) {
+            var currentChar = textBalance.toCharArray()[i];
+            if (currentChar == '.') {//TODO: DATA MODEL
+                window.setItem(i, new Item("Баланс", textBalance, Material.PAPER, 1, 17000));
+                continue;
+            }
+
+            window.setItem(i, new Item("Баланс", textBalance, Material.PAPER, 1,
+                    17000 + Integer.parseInt(currentChar+""))); //TODO: DATA MODEL
+        }
+
+        for (var i = 0; i < 8; i++) {
             if (i == 0 && playerBalance < enableBalance)
-                window.setItem(0, new Item(new ItemStack(Material.GREEN_WOOL),
-                        Math.round(playerBalance * 100) / 100 + CashManager.currencySigh));
+                window.setItem(41, new Item("Снять максимум", //TODO: DATA MODEL
+                        Math.round(playerBalance * 100) / 100 + CashManager.currencySigh, Material.PAPER, 1, 17000));
 
             if (enableBalance >= ChangeManager.Denomination.get(i) && playerBalance >= ChangeManager.Denomination.get(i))
-                for (var j = i; j < 8; j++)
-                    window.setItem(j + 1, new Item(new ItemStack(Material.GREEN_WOOL),
-                            ChangeManager.Denomination.get(j) + CashManager.currencySigh));
+                printButtons(41, window);
+
+            if (i == 0)
+                window.setItem(36, new Item("Положить все средства", //TODO: DATA MODEL
+                        "-" + cashInInventory + CashManager.currencySigh, Material.PAPER, 1, 17000));
+
+            if (cashInInventory >= ChangeManager.Denomination.get(i))
+                printButtons(36, window);
         }
 
         return window;
     }
 
-    public static Inventory putWindow(Player player) {
-        var window = Bukkit.createInventory(player, 9, Component.text("Банк-Внесение"));
-        var cashInInventory = new CashManager().amountOfCashInInventory(player);
-
-        for (var i = 0; i < 9; i++) {
-            if (i == 0)
-                window.setItem(0, new Item(new ItemStack(Material.RED_WOOL),
-                        "-" + cashInInventory + CashManager.currencySigh));
-
-            if (cashInInventory >= ChangeManager.Denomination.get(i))
-                for (var j = i; j < 8; j++)
-                    window.setItem(j + 1, new Item(new ItemStack(Material.RED_WOOL),
-                            "-" + ChangeManager.Denomination.get(j) + CashManager.currencySigh));
-        }
-
-        return window;
+    private void printButtons(int position, Inventory window) {
+        for (var j = 1; j < 8; j++) //TODO: DATA MODEL
+            window.setItem(j + (position + 5 * (j / 4)), new Item(new ItemStack(Material.RED_WOOL),
+                    (position == 36 ? "-" : "") + ChangeManager.Denomination.get(j) + CashManager.currencySigh));
     }
 }
